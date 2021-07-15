6d200711338857fc44167adaed45c04395af3b9b
Phong <v.ducphong@gmail.com>
2021-07-15 16:09:00 +0300

    added ActivityIndicator to our app in restaurant screen

--- src/features/restaurants/screens/restaurants.screen..js
+++ src/features/restaurants/screens/restaurants.screen..js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
 import { StatusBar, FlatList, SafeAreaView, View } from "react-native";
 import styled from "styled-components/native";
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
@@ -22,18 +23,37 @@ const RestaurantList = styled(FlatList).attrs({
     }
 })``;
 
+const Loading = styled(ActivityIndicator)`
+    margin-left: -25px;
+`;
+
+const LoadingContainer = styled(View)`
+    position: absolute;
+    top: "50%";
+    left: "50%"; 
+`;
+
 export const RestaurantsScreen = () => {
     const { isLoading, error, restaurants } = useContext(RestaurantContext);
-
     return (
         <SafeArea>
+            {
+                isLoading && (
+                    <LoadingContainer>
+                        <Loading
+                            size={50}
+                            animating={true}
+                            color={Colors.blue300}
+                        />
+                    </LoadingContainer>
+                )
+            }
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
             <RestaurantList
                 data={restaurants}
                 renderItem={({ item }) => {
-                    console.log(item);
                     return (<>
                         <Spacer position='bottom' size='large' />
                         <RestaurantInfoCard restaurant={item} />
