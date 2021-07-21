4fe690ffc3abd0d150f4c74d884682be885f61e9
Phong <v.ducphong@gmail.com>
2021-07-21 07:59:02 +0300

    implemented firebase and initialized firebase

--- App.js
+++ App.js
@@ -11,6 +11,20 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastruture/navigation";
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCG-VnlnobLlJAOUvKYsuSxmCNZSveYnYg",
+  authDomain: "mealstogo-4bcab.firebaseapp.com",
+  projectId: "mealstogo-4bcab",
+  storageBucket: "mealstogo-4bcab.appspot.com",
+  messagingSenderId: "202467037900",
+  appId: "1:202467037900:web:f65cfd183e75e5a04fa869",
+  measurementId: "G-GNGJSN6H6L"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
