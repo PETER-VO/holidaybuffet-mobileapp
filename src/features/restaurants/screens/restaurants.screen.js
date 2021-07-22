074ec461b1d771ffb33c0d5a286a579d5430ceae
Phong <v.ducphong@gmail.com>
2021-07-22 16:16:23 +0300

    adding fade animation and icon of our app

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { FlatList, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 
@@ -11,16 +11,12 @@ import { RestaurantsContext } from "../../../services/restaurants/restaurants.co
 import { Search } from "../components/search.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
+import { RestaurantList } from "../components/restaurant-list.styles";
+import { FadeInView } from "../../../components/animations/fade.animation";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
-
-const RestaurantList = styled(FlatList).attrs({
-    contentContainerStyle: {
-        padding: 16,
-    },
-})``;
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
@@ -57,7 +53,9 @@ export const RestaurantsScreen = ({ navigation }) => {
                             onPress={() => navigation.navigate('RestaurantDetail', { restaurant: item })
                             }>
                             <Spacer position="bottom" size="large">
-                                <RestaurantInfoCard restaurant={item} />
+                                <FadeInView>
+                                    <RestaurantInfoCard restaurant={item} />
+                                </FadeInView>
                             </Spacer>
                         </TouchableOpacity>
                     );
--- src/infrastruture/navigation/settings.navigator.js
+++ src/infrastruture/navigation/settings.navigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import { SettingScreen } from '../../features/settings/screens/settings.screen';
+import { FavouritesScreen } from '../../features/settings/screens/favourties.screen';
 
 
 const SettingsStack = createStackNavigator();
@@ -20,7 +21,7 @@ export const SettingNavigator = ({ route, navigation }) => {
                 name="Settings"
                 component={SettingScreen}
             />
-            <SettingsStack.Screen name="Favourites" component={() => null} />
+            <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
         </SettingsStack.Navigator>
     )
 }
